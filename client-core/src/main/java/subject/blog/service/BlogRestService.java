43a2066055f394771416249dbd69b2a069143778
jhsong2580 <thdwmdgns@naver.com>
2023-03-19 12:24:52 +0900

    fix: BlogRestService의 블로그 받아오는 정보 List가 아닌 객체로 변경

--- client-core/src/main/java/subject/blog/service/BlogRestService.java
+++ client-core/src/main/java/subject/blog/service/BlogRestService.java
@@ -1,10 +1,9 @@
 package subject.blog.service;
 
-import java.util.List;
+import subject.blog.dto.BlogListResponseDTO;
 import subject.blog.dto.BlogRequestDTO;
-import subject.blog.dto.BlogResponseDTO;
 
 public interface BlogRestService {
 
-    List<BlogResponseDTO> getBlogs(BlogRequestDTO blogRequestDTO);
+    BlogListResponseDTO getBlogs(BlogRequestDTO blogRequestDTO);
 }
