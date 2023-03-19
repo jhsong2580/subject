67e54c2655cc301275a7485efa8403e756342a83
jhsong2580 <thdwmdgns@naver.com>
2023-03-19 22:15:38 +0900

    feat: Blog Search를 사용하는 사용자에게 받는 QueryString 객체 생성

---
+++ app-blog/src/main/java/subject/blog/dto/request/BlogSearchRequestForm.java
@@ -0,0 +1,34 @@
+package subject.blog.dto.request;
+
+import javax.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+import subject.blog.enums.Sort;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class BlogSearchRequestForm {
+
+    @NotEmpty(message = "query field must be required")
+    private String query;
+
+    private Sort sort;
+
+    @Range(min = 1, max = 50, message = "page field is a value between 1 and 50")
+    private int page;
+
+    @Range(min = 10, max = 50, message = "size field is a value between 10 and 50")
+    private int size;
+
+    @Builder
+    public BlogSearchRequestForm(String query, Sort sort, Integer page, Integer size) {
+        this.query = query;
+        this.sort = sort != null ? sort : Sort.ACCURACY;
+        this.page = page != null ? page : 1;
+        this.size = size != null ? size : 10;
+    }
+}
