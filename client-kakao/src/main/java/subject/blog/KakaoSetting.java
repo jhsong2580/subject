8b3dfb85bf900e07506a89c6e170783c33b90d47
jhsong2580 <thdwmdgns@naver.com>
2023-03-19 12:59:07 +0900

    fix: Kakao Setting을 받아오기 위해 누락된 @Component 추가

--- client-kakao/src/main/java/subject/blog/KakaoSetting.java
+++ client-kakao/src/main/java/subject/blog/KakaoSetting.java
@@ -2,9 +2,11 @@ package subject.blog;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import subject.blog.config.KakaoConfig;
 
 @ComponentScan
+@Component
 @EnableConfigurationProperties({KakaoConfig.class})
 public class KakaoSetting {
 
