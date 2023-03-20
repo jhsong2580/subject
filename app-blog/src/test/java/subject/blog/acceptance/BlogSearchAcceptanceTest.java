423080b8b8e66ae7b08d17134884476a549310c0
jhsong2580 <thdwmdgns@naver.com>
2023-03-20 23:19:36 +0900

    test: 입력값 검증 인수테스트 추가

--- app-blog/src/test/java/subject/blog/acceptance/BlogSearchAcceptanceTest.java
+++ app-blog/src/test/java/subject/blog/acceptance/BlogSearchAcceptanceTest.java
@@ -10,7 +10,10 @@ import io.restassured.response.Response;
 import java.util.Arrays;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import subject.blog.acceptance.utils.MockSettingDTO;
@@ -36,11 +39,11 @@ public class BlogSearchAcceptanceTest extends AcceptanceTest {
         네이버_블로그내역조회_성공_MockServer_구동();
 
         //when
-        ExtractableResponse<Response> 응답 = Blog_정보조회_요청하기("query", 5, 1, Sort.ACCURACY);
+        ExtractableResponse<Response> 응답 = Blog_정보조회_요청하기("query", 10, 1, Sort.ACCURACY);
 
         //then
         블로그_컨텐츠_조회됨(응답,
-            5, 1,
+            10, 1,
             "kakaoContent1",
             "kakaoContent2",
             "kakaoContent3",
@@ -56,11 +59,11 @@ public class BlogSearchAcceptanceTest extends AcceptanceTest {
         네이버_블로그내역조회_성공_MockServer_구동();
 
         //when
-        ExtractableResponse<Response> 응답 = Blog_정보조회_요청하기("query", 5, 1, Sort.ACCURACY);
+        ExtractableResponse<Response> 응답 = Blog_정보조회_요청하기("query", 10, 1, Sort.ACCURACY);
 
         //then
         블로그_컨텐츠_조회됨(응답,
-            5, 1,
+            10, 1,
             "naverContent1",
             "naverContent2",
             "naverContent3",
@@ -72,16 +75,56 @@ public class BlogSearchAcceptanceTest extends AcceptanceTest {
     public void 카카오_네이버_전부_장애일때_블로그_조회() {
         //given
         카카오_블로그내역조회_실패_MockServer_구동();
-        startNaverMockServerGetBlogListFail();
+        네이버_블로그내역조회_실패_MockServer_구동();
 
         //when
-        ExtractableResponse<Response> response = Blog_정보조회_요청하기("query", 5, 1, Sort.ACCURACY);
+        ExtractableResponse<Response> 응답 = Blog_정보조회_요청하기("query", 10, 1, Sort.ACCURACY);
 
         //then
         assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(
+            () -> assertThat(응답.statusCode()).isEqualTo(
                 HttpStatus.INTERNAL_SERVER_ERROR.value())
         );
+    }
+
+    @DisplayName("page size는 10~50 사이의 값이다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+        "9:400",
+        "10:200",
+        "15:200",
+        "50:200",
+        "51:400"
+    }, delimiter = ':')
+    public void 블로그조회_사이즈제한_테스트(int size, int httpStatusCode) {
+        //given
+        카카오_블로그내역조회_성공_MockServer_구동();
+
+        //when
+        ExtractableResponse<Response> 응답 = Blog_정보조회_요청하기("query", size, 1, Sort.ACCURACY);
+
+        //then
+        assertThat(응답.statusCode()).isEqualTo(httpStatusCode);
+    }
+
+    @DisplayName("page 번호는 1~50 사이의 값이다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+        "0:400",
+        "1:200",
+        "15:200",
+        "50:200",
+        "51:400"
+    }, delimiter = ':')
+    public void 블로그조회_페이지제한_테스트(int page, int httpStatusCode) {
+        //given
+        카카오_블로그내역조회_성공_MockServer_구동();
+
+        //when
+        ExtractableResponse<Response> 응답 = Blog_정보조회_요청하기("query", 10, page, Sort.ACCURACY);
+
+        //then
+        assertThat(응답.statusCode()).isEqualTo(httpStatusCode);
     }
 
     private void 카카오_블로그내역조회_성공_MockServer_구동() {
@@ -205,7 +248,7 @@ public class BlogSearchAcceptanceTest extends AcceptanceTest {
         super.addMockResponse(Arrays.asList(kakaoGetBlogListSetting), super.kakaoMockPort);
     }
 
-    private void startNaverMockServerGetBlogListFail() {
+    private void 네이버_블로그내역조회_실패_MockServer_구동() {
         MockSettingDTO naverGetBlogListSetting = new MockSettingDTO(super.kakaoBlogListEndpoint,
             HttpMethod.GET,
             HttpStatus.BAD_REQUEST,
