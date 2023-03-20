c37346d84d4d9ff4fdea92b148a5ad0b6188fe26
jhsong2580 <thdwmdgns@naver.com>
2023-03-20 23:19:16 +0900

    feat: 입력값 오류시 Throw IllegalArgument Exception

--- app-blog/src/main/java/subject/blog/controller/BlogController.java
+++ app-blog/src/main/java/subject/blog/controller/BlogController.java
@@ -1,8 +1,10 @@
 package subject.blog.controller;
 
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -26,6 +28,16 @@ public class BlogController {
         @Validated @ModelAttribute BlogSearchRequestForm blogSearchRequestForm,
         BindingResult bindingResult
     ) {
+
+        if(bindingResult.hasErrors()) {
+            throw new IllegalArgumentException(
+                bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.toList())
+                    .toString()
+            );
+        }
 
         BlogRequestDTO blogRequestDTO = toBlogRequestDTOMapper.to(blogSearchRequestForm);
 
